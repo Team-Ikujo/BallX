9ee52a628761ef522a959d1ea33ea1a60bd51aea
junsang-ee <dkwnstkddk@gmail.com>
2026-01-20 19:15:06 +0900

    [INIT] Init springboot project

---
+++ backend/src/main/java/com/ballx/BallxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ballx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallxBackendApplication.class, args);
+	}
+
+}
