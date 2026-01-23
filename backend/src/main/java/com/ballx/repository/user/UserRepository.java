69a81297d1cc0ced002e2052e1604a9be8891243
junsang-ee <dkwnstkddk@gmail.com>
2026-01-23 10:48:51 +0900

    [BE] feat: #8 회원(Member) 및 유저(User) 레파지토리 생성

---
+++ backend/src/main/java/com/ballx/repository/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ballx.repository.user;
+
+import com.ballx.domain.entity.user.UserEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
