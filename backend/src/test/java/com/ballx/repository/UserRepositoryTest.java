44c2b4287b66071c8e6ee33a9276bd08531923b7
junsang-ee <dkwnstkddk@gmail.com>
2026-01-23 11:49:49 +0900

    [BE] feat: #8 유저id,회원id 비교로직 추가 (UserRepositoryTest)

--- backend/src/test/java/com/ballx/repository/UserRepositoryTest.java
+++ backend/src/test/java/com/ballx/repository/UserRepositoryTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @DataJpaTest
@@ -39,10 +39,13 @@ public class UserRepositoryTest {
 
 	@Test
 	void 유저_저장_테스트() {
+		assertNotNull(member.getId());
 		UserEntity user = userRepository.findById(member.getId()).orElse(null);
 		assertNotNull(user);
 		assertNotNull(user.getId());
+		assertEquals(member.getId(), user.getId());
 		log.info("user id : {}", user.getId());
 		log.info("member id : {}", member.getId());
+		log.info("user role : {}", user.getRole());
 	}
 }
