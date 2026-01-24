c6a0356cbfa19a21f52e11306d76bbd130ad00b0
junsang-ee <dkwnstkddk@gmail.com>
2026-01-24 17:48:32 +0900

    [BE] refactor: #13 ErrorCode: 타입 관련 에러 추가 (타입오류, 필수 파라미터 누락 및 형식 오류) ErrorCode: 서버 내부 오류 추가 (5xx)

--- backend/src/main/java/com/ballx/constants/messages/ErrorCode.java
+++ backend/src/main/java/com/ballx/constants/messages/ErrorCode.java
@@ -10,9 +10,18 @@ import java.text.MessageFormat;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+	// Client Error (4xx)
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 	INVALID_DOMAIN_FIELD(HttpStatus.BAD_REQUEST, "도메인 필드 오류 : {0}"),
-	BODY_FIELD_ERROR(HttpStatus.BAD_REQUEST, "바디 필드 오류 : {0}");
+	BODY_FIELD_ERROR(HttpStatus.BAD_REQUEST, "바디 필드 오류 : {0}"),
+
+	// 타입 관련 Error
+	TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "{0} 타입 오류"),
+	MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "{0} 파라미터 필요"),
+	INVALID_FORMAT(HttpStatus.BAD_REQUEST, "{0} 형식 오류"),
+
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.");
+
 	private final HttpStatus status;
 	private final String message;
 
