71ac28713a08ff9f68b07ce667b14a75a9f84cd7
junsang-ee <dkwnstkddk@gmail.com>
2026-01-26 15:12:03 +0900

    [BE] feat: #19 회원도메인(MemberEntity) validate() 검증 로직 생성

--- backend/src/main/java/com/ballx/domain/entity/user/MemberEntity.java
+++ backend/src/main/java/com/ballx/domain/entity/user/MemberEntity.java
@@ -5,11 +5,15 @@ import com.ballx.constants.MemberStatus;
 
 import com.ballx.constants.UserRole;
 
+import com.ballx.validation.Preconditions;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import org.springframework.util.StringUtils;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -34,6 +38,21 @@ public class MemberEntity extends UserEntity {
 		final String mobile,
 		final Gender gender
 	) {
+		validate(name, mobile, gender);
 		return new MemberEntity(name, mobile, gender);
+	}
+
+	private static void validate(String name, String mobile, Gender gender) {
+		Preconditions.domainValidate(
+			StringUtils.hasText(name), "회원 이름은 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			StringUtils.hasText(mobile), "회원 휴대전화 번호는 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			gender != null, "회원 성별은 비어 있을 수 없습니다."
+		);
 	}
 }
