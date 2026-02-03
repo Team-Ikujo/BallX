604e32dcadb47c6a95b5627d5ced992e05268a93
AkiStory <sonsungjeon@gmail.com>
2026-02-03 11:51:36 +0900

    [BE] feat: #33 빠진 exception 과 enum

---
+++ backend/src/main/java/com/ballx/constants/OAuthProvider.java
@@ -0,0 +1,14 @@
+package com.ballx.constants;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum OAuthProvider {
+	GOOGLE("구글"),
+	KAKAO("카카오"),
+	NAVER("네이버");
+
+	private final String registrationId;
+}
