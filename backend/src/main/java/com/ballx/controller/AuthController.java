59d798c6380301bb5bc98ae2745f2e1df00d746c
AkiStory <sonsungjeon@gmail.com>
2026-02-06 04:03:48 +0900

    BE feat: #53 소셜로그인 액세스토큰 발급을 위한 컨트롤러

--- backend/src/main/java/com/ballx/controller/AuthController.java
+++ backend/src/main/java/com/ballx/controller/AuthController.java
@@ -3,12 +3,16 @@ package com.ballx.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ballx.common.api.ApiSuccessResponse;
 import com.ballx.constants.ProviderType;
+import com.ballx.domain.dto.request.social.SocialLoginRequest;
 import com.ballx.domain.dto.response.social.SocialCallbackResponse;
+import com.ballx.domain.dto.response.social.SocialTokenInfoResponse;
 import com.ballx.service.social.SocialAuthService;
 
 import jakarta.validation.Valid;
@@ -28,6 +32,19 @@ public class AuthController {
 		ProviderType providerType = ProviderType.upperProviderType(provider);
 
 		SocialCallbackResponse response = socialAuthService.getCode(providerType, code);
+
+		return ApiSuccessResponse.wrap(response);
+	}
+
+	@PostMapping("/api/v1/auth/{provider}/login")
+
+	public ResponseEntity<ApiSuccessResponse<SocialTokenInfoResponse>> getSocialAccessToken(
+		@PathVariable String provider,
+		@Valid @RequestBody SocialLoginRequest request
+	) {
+		ProviderType providerType = ProviderType.upperProviderType(provider);
+
+		SocialTokenInfoResponse response = socialAuthService.getAccessToken(providerType, request.code());
 
 		return ApiSuccessResponse.wrap(response);
 	}
