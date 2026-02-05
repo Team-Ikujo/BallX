d811ec109f9fa864f53e901e922151ab78d2aaa1
AkiStory <sonsungjeon@gmail.com>
2026-02-05 15:32:36 +0900

    BE feat: #53 Social Access Token receive

--- backend/src/main/java/com/ballx/config/properties/SocialProperties.java
+++ backend/src/main/java/com/ballx/config/properties/SocialProperties.java
@@ -1,31 +1,36 @@
 package com.ballx.config.properties;
 
 import java.util.Map;
-import java.util.Set;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.security.oauth2.client")
 public record SocialProperties(
-	Map<String, Provider> provider,
-	Map<String, Registration> registration
+	Map<String, Registration> registration,
+	Map<String, Provider> provider
 ) {
 	public record Registration(
-		String authorizationGrantType,
-		String clientAuthenticationMethod,
 		String clientId,
-		String clientName,
 		String clientSecret,
 		String redirectUri,
-		Set<String> scope
+		String authorizationGrantType,
+		String clientAuthenticationMethod
 	) {
 	}
 
 	public record Provider(
 		String authorizationUri,
-		String issuerUri,
-		String jwkSetUri,
 		String tokenUri,
-		String UserInfoAuthenticationMethod,
 		String userInfoUri,
 		String userNameAttribute
 	) {
+	}
+
+	public Registration getRegistration(String provider) {
+		return this.registration.get(provider);
+	}
+
+	public Provider getProvider(String provider) {
+		return this.provider.get(provider);
 	}
 }
