ef881f10ec82d2c4e19d18e6b432b598d98dc89b
AkiStory <sonsungjeon@gmail.com>
2026-02-06 01:52:10 +0900

    BE refactor: #53 @불필요한 어노테이션 제거

--- backend/src/main/java/com/ballx/BallxBackendApplication.java
+++ backend/src/main/java/com/ballx/BallxBackendApplication.java
@@ -3,13 +3,11 @@ package com.ballx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableConfigurationProperties
 public class BallxBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BallxBackendApplication.class, args);
