45fc3cc2dbcc34fae0ebaf8c9cc07dda65c63685
AkiStory <sonsungjeon@gmail.com>
2026-02-06 04:03:35 +0900

    BE feat: #53 소셜로그인 액세스토큰 발급을 위한 서비스로직

--- backend/src/main/java/com/ballx/service/social/SocialAuthService.java
+++ backend/src/main/java/com/ballx/service/social/SocialAuthService.java
@@ -1,10 +1,19 @@
 package com.ballx.service.social;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ballx.constants.ProviderType;
+import com.ballx.constants.messages.ErrorCode;
 import com.ballx.domain.dto.response.social.SocialCallbackResponse;
+import com.ballx.domain.dto.response.social.SocialTokenInfoResponse;
+import com.ballx.exception.CustomException;
+import com.ballx.infra.social.SocialAuthProvider;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +22,28 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class SocialAuthService {
 
+	private final Map<String, SocialAuthProvider> providerMap;
+
+	public SocialAuthService(List<SocialAuthProvider> providers) {
+		this.providerMap = providers.stream()
+			.collect(Collectors.toMap(
+					SocialAuthProvider::getProviderName,
+					Function.identity()
+				)
+			);
+	}
+
 	public SocialCallbackResponse getCode(ProviderType providerType, String code) {
 		return SocialCallbackResponse.of(providerType, code);
+	}
+
+	public SocialTokenInfoResponse getAccessToken(ProviderType providerType, String code) {
+		SocialAuthProvider provider = providerMap.get(providerType.getProviderId());
+
+		if (provider == null) {
+			throw new CustomException(ErrorCode.SOCIAL_PROVIDER_NOT_SUPPORTED);
+		}
+
+		return provider.getAccessToken(code);
 	}
 }
