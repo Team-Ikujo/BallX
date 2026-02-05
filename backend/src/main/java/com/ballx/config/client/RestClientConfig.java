f2069bda03ccffa22aef49b2f93f2f1891cfe1af
suyeon89 <whtndus0809@naver.com>
2026-02-05 16:49:01 +0900

    [BE] feat:
    RestClientConfig 작성

---
+++ backend/src/main/java/com/ballx/config/client/RestClientConfig.java
@@ -0,0 +1,57 @@
+package com.ballx.config.client;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import com.ballx.config.client.interceptor.ExternalLoggingInterceptor;
+import com.ballx.config.client.interceptor.ExternalTraceInterceptor;
+import com.ballx.config.properties.RestClientProperties;
+import com.ballx.exception.client.handler.ExternalErrorHandler;
+
+@Configuration
+@EnableConfigurationProperties(RestClientProperties.class)
+public class RestClientConfig {
+
+	@Bean
+	public ExternalErrorHandler externalErrorHandler() {
+		return new ExternalErrorHandler();
+	}
+
+	@Bean
+	public ExternalTraceInterceptor externalTraceInterceptor() {
+		return new ExternalTraceInterceptor();
+	}
+
+	@Bean
+	public ExternalLoggingInterceptor externalLoggingInterceptor() {
+		return new ExternalLoggingInterceptor();
+	}
+
+	@Bean
+	public RestClient externalRestClient(
+		RestClientProperties props,
+		ExternalErrorHandler errorHandler,
+		ExternalTraceInterceptor traceInterceptor,
+		ExternalLoggingInterceptor loggingInterceptor
+	) {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout((int) props.connectTimeoutMs());
+		factory.setReadTimeout((int) props.readTimeoutMs());
+
+		return RestClient.builder()
+			.requestFactory(factory)
+			.defaultStatusHandler(
+				HttpStatusCode::isError,
+				errorHandler
+			)
+			.requestInterceptor(traceInterceptor)
+			.requestInterceptor(loggingInterceptor)
+			.build();
+	}
+}
