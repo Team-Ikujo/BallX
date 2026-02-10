218609fd0358c0bd95a2050bc0245ef00969e782
junsang-ee <dkwnstkddk@gmail.com>
2026-02-10 18:35:52 +0900

    [BE] feat: #61 Oauth(kakao, naver, google) AccessToken Request Dto (소셜api 요청 dto) 추가 기존 SecurityPath 수정

--- backend/src/main/java/com/ballx/security/SecurityPath.java
+++ backend/src/main/java/com/ballx/security/SecurityPath.java
@@ -3,19 +3,7 @@ package com.ballx.security;
 public final class SecurityPath {
 
 	public static final String[] PUBLIC = {
-		"/oauth2/redirect",
-
-		// OAuth2 Provider
-		"/auth/*/callback",
-		"/auth/google/callback",
-		"/auth/kakao/callback",
-		"/auth/naver/callback",
-
-		// 소셜로그인
-		"/api/v1/auth/*/login",
-		"/api/v1/auth/google/login",
-		"/api/v1/auth/kakao/login",
-		"/api/v1/auth/naver/login",
+		"/api/v1/auth/**",
 	};
 
 }
