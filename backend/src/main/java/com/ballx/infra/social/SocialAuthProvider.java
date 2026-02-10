3512c392d9d21ec7d965633c4196669f42ad6a7c
junsang-ee <dkwnstkddk@gmail.com>
2026-02-10 18:23:13 +0900

    [BE] feat: #61 기존 SocialAuthProvider 제거 기존 SocialAuthService 제거 기존 AuthProviders(naver,google,kakao)제거

--- backend/src/main/java/com/ballx/infra/social/SocialAuthProvider.java
+++
@@ -1,9 +0,0 @@
-package com.ballx.infra.social;
-
-import com.ballx.domain.dto.response.social.SocialTokenInfoResponse;
-
-public interface SocialAuthProvider {
-	SocialTokenInfoResponse getAccessToken(String code);
-
-	String getProviderName();
-}
